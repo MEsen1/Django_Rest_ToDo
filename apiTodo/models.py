199cc91bb8c6b2fa82e7d56032524c7d5c7bb918
MEsen1 <mehmet.esen.94@gmail.com>
2022-03-15 20:26:12 +0000

    model-todo

---
+++ apiTodo/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    TITLE = {
+        ('H','High')
+        ('M','Medium')
+        ('L','Low')
+        
+    }
+    
+    priority = models.CharField(max_length=50,choices= TITLE)
+    done= models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.task}'
+    
